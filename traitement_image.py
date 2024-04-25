a23ef7b912914ce37bd7736dbf66da5fa6645e26
Mathieu <mathieu.gouleau@scalian.com>
2024-04-25 16:51:28 +0200

    mask

--- traitement_image.py
+++ traitement_image.py
@@ -1,8 +1,15 @@
 import numpy as np
 import cv2
 
-image = cv2.imread('images/nevermind.png',-1)
+image = cv2.imread('images/pomme.jpg',-1)
 
-cv2.imshow('Image avec ligne', image)
+image_hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+
+cv2.namedWindow('Image HSV', cv2.WINDOW_NORMAL)
+
+cv2.resizeWindow('Image HSV', 500, 400)
+
+
+cv2.imshow('Image HSV', image_hsv)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

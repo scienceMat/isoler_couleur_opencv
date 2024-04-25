23185b18fd50731a5bba6ae82632674a781cfe2e
Mathieu <mathieu.gouleau@scalian.com>
2024-04-25 16:52:21 +0200

    mask_2

--- traitement_image.py
+++ traitement_image.py
@@ -5,11 +5,16 @@ image = cv2.imread('images/pomme.jpg',-1)
 
 image_hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
+lower_red = np.array([168,25,25])
+upper_red  = np.array([180,255,255])
+
+mask = cv2.inRange(image_hsv, lower_red, upper_red)
+
 cv2.namedWindow('Image HSV', cv2.WINDOW_NORMAL)
 
 cv2.resizeWindow('Image HSV', 500, 400)
 
 
-cv2.imshow('Image HSV', image_hsv)
+cv2.imshow('Image HSV', mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

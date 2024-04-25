30a135521e116a41a0a862691082a654b795277d
Mathieu <mathieu.gouleau@scalian.com>
2024-04-25 16:54:09 +0200

    detection couleur

--- traitement_image.py
+++ traitement_image.py
@@ -8,13 +8,15 @@ image_hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 lower_red = np.array([168,25,25])
 upper_red  = np.array([180,255,255])
 
-mask = cv2.inRange(image_hsv, lower_red, upper_red)
+maskRED = cv2.inRange(image_hsv, lower_red, upper_red)
+
+result_red = cv2.bitwise_and(image,image,mask=maskRED)
 
 cv2.namedWindow('Image HSV', cv2.WINDOW_NORMAL)
 
 cv2.resizeWindow('Image HSV', 500, 400)
 
 
-cv2.imshow('Image HSV', mask)
+cv2.imshow('Image HSV', result_red)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
